fbe3b7e03ed56b0391e38515104510cdb26be56a
jyj1143 <54170515+jyj1143@users.noreply.github.com>
2022-07-23 06:03:58 +0900

    Add files via upload

---
+++ model/posts/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+app_name = 'posts'
+urlpatterns = [
+     # /posts/
+    path('', views.index, name='index'),
+
+    # /posts/create/
+    path('create/', views.post_create, name='post_create'),
+    
+    # /posts/1/comment_create/
+    path('<int:post_id>/comment_create', views.comment_create, name="comment_create"),
+]
